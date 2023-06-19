1e05159779e58c86442a348fa519a55c4da626ed
ash604 <amveevers@gmail.com>
2023-06-19 16:09:19 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -10,9 +10,8 @@ if (temperature < 0) {
 
 console.log("Now you're ready to go outside!");
 
-
 const isCitizen = true;
-const age = 26;
+let age = 26;
 
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
@@ -26,3 +25,18 @@ if (!raining) {
   console.log("Leave your umbrella at home!");
 }
 
+
+
+let age = 15
+let whichSchool = ""
+
+if (age < 13)  {
+  whichSchool = "elementary";
+} else if (age >= 13 && age <= 18) {
+  whichSchool = "Secondary School";
+} else if (age > 18) {
+  whichSchool = "Lighthouse labs";
+  
+}
+
+console.log("At " + age + ", you should go to " + whichSchool + "!");
